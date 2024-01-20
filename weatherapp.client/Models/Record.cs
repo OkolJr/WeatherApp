79366ed9d2c67ea705b40b5cacb9f01721a079e0
OkolJr <ibrahim.060308@gmail.com>
2024-01-20 08:49:27 +0200

    Update Record

---
+++ weatherapp.client/Models/Record.cs
@@ -0,0 +1,13 @@
+public class Record : IHasGuidId, IHasDateStamp, IHasSoftDelete
+{
+  public Guid Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime? DeletedAt { get; set; }
+  public string City { get; set; } = string.Empty;
+  public double Lat { get; set; } = 0;
+  public double Lon { get; set; } = 0;
+  public string Country {  get; set; } = string.Empty;
+  public string State {  get; set; } = string.Empty;
+  public CurrentWeather? CurrentWeather { get; set; }
+  public FiveDaysWeather? FiveDaysWeather { get; set; }
+}
