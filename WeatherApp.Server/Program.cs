fc21682827655fff4a7c1ebb1a6ac16e3b6da52f
OkolJr <151054946+OkolJr@users.noreply.github.com>
2024-01-27 09:35:58 +0200

    Reconfiging data
    Edited appsetings and created DTOs

--- WeatherApp.Server/Program.cs
+++ WeatherApp.Server/Program.cs
@@ -1,34 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddTransient<HttpClient>(); // this is kind of DI, but used in no production way
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.CustomSchemaIds(type => type.FullName); // Use the type's full name as the schema ID
-    // Other Swagger configuration...
-});
-
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
-
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
-
 app.Run();
